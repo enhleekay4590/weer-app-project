b254ee5c4b0126ada0a9ee326b25e6b96c8ea8c0
EnhleeKay <minenhle.kubheka241@gmail.com>
2024-01-29 15:39:06 +0200

    Updated the days of the week, converted timestamp to accurate day. Updated links and weather icons

--- src/index.js
+++ src/index.js
@@ -55,6 +55,13 @@ function searchElement(event) {
   searchForCity(searchInputElement.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "003ed39ao8f2b2339d6b100400t60f78";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -62,24 +69,29 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">⛅</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div class="weather-forecast-icon">
+          <img src="${day.condition.icon_url}" class="image-icon" />
+        </div>
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>34°</strong>
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
           </div>
-          <div class="weather-forecast-temperature">22°</div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}°</div>
         </div>
       </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
